f6b7cacad73bee318048f2304fae6901c398fe88
OPTImist2846 <olessolja@gmail.com>
2025-04-28 01:01:49 +0300

    class Priduct, def, copy, move

---
+++ Product.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oless on 4/28/2025.
+//
+
+#include "Product.h"
---
+++ Product.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+using namespace std;
+
+class Product {
+protected:
+    string name;
+    double price;
+
+public:
+    Product(const string& name, double price) : name(name), price(price) {
+        cout << "Construct Product: " << this->name << endl;
+    }
+
+    Product(const Product& other) : name(other.name), price(other.price) {
+        cout << "Copy Product: " << this->name << endl;
+    }
+
+    Product(Product&& other) noexcept : name(move(other.name)), price(other.price) {
+        cout << "Move Product: " << this->name << endl;
+    }
+
+};
+#endif //PRODUCT_H
