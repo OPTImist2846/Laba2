5457f4c0ebf8712cd009d5b3f155e4b3682b151b
OPTImist2846 <olessolja@gmail.com>
2025-05-25 20:52:58 +0300

    optimisation Plant.h (out komment, cout)

--- Plant.h
+++ Plant.h
@@ -14,47 +14,21 @@ protected:
 
 public:
 
-    Plant() : name("Unknown"), color("Unknown") {
-        cout << "Plant default constructor" << endl;
-    }
+    Plant();
+    Plant(const std::string& name, const string& color);
+    Plant(const Plant& other);
+    Plant& operator=(const Plant& other);
+    virtual ~Plant();
 
-    // Конструктор з параметрами назви та кольору
-    Plant(const string& name, const string& color) : name(name), color(color) {
-        cout << "Plant constructor: " << name << endl;
-    }
-
-    // Конструктор копіювання
-    Plant(const Plant& other) : name(other.name), color(other.color) {
-        cout << "Plant copy constructor" << endl;
-    }
-
-    // Оператор присвоєння копіюванням
-    Plant& operator=(const Plant& other) {
-        cout << "Plant assignment operator" << endl;
-        if (this != &other) {
-            name = other.name;
-            color = other.color;
-        }
-        return *this;
-    }
-
-    // віртуальний деструктор
-    virtual ~Plant() {
-        cout << "Plant destructor: " << name << endl;
-    }
-
-    // oтримання назви та кольору
-    string getName() const { return name; }
-    string getColor() const { return color; }
+    string getName() const;
+    string getColor() const;
 
 
-    // виведення інформації про рослину
-    virtual void displayInfo() const {
-        cout << "Name: " << name << ", Color: " << color;
-    }
-    virtual void photosynthesis() const {
-        cout << "Plant is performing photosynthesis" << endl;
-    }
+
+    virtual void displayInfo() const;
+    virtual void photosynthesis() const;
+
+    virtual Plant* clone() const = 0;
 };
 
 
