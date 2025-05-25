3858cb1186f05c5c4ee179cf8a6d9107a57fe25a
OPTImist2846 <olessolja@gmail.com>
2025-05-25 20:53:58 +0300

    add Plant.cpp

--- Plant.cpp
+++ Plant.cpp
@@ -1,5 +1,17 @@
-//
-// Created by oless on 5/11/2025.
-//
 
 #include "Plant.h"
+Plant::Plant() : name("Unknown"), color("Unknown") {}
+Plant::Plant(const std::string& name, const std::string& color) : name(name), color(color) {}
+Plant::Plant(const Plant& other) : name(other.name), color(other.color) {}
+Plant& Plant::operator=(const Plant& other) {
+    if (this != &other) {
+        name = other.name;
+        color = other.color;
+    }
+    return *this;
+}
+Plant::~Plant() {}
+std::string Plant::getName() const { return name; }
+std::string Plant::getColor() const { return color; }
+void Plant::displayInfo() const { std::cout << "Name: " << name << ", Color: " << color; }
+void Plant::photosynthesis() const { std::cout << "Plant is performing photosynthesis" << std::endl; }
