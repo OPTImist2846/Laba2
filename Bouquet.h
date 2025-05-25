fe94150c458b66d6ddecd06cd4af5eeb2d211349
OPTImist2846 <olessolja@gmail.com>
2025-05-25 21:00:51 +0300

    optimisation Bouquet.h

--- Bouquet.h
+++ Bouquet.h
@@ -13,62 +13,18 @@ private:
     vector<Flower> flowers; // Вектор, що зберігає квіти в букеті
 
 public:
-    Bouquet() : name("No name") {
-        cout << "Bouquet default constructor called" << endl;
-    }
+    Bouquet();
+    Bouquet(const string& name);
+    Bouquet(const Bouquet& other); // Конструктор копіювання
+    Bouquet& operator=(const Bouquet& other); // Оператор присвоєння копіюванням
+    ~Bouquet();
 
-    // Конструктор з назвою букета
-    Bouquet(const string& name) : name(name) {
-        cout << "Bouquet constructor with name: " << name << endl;
-    }
 
-    // Конструктор копіювання
-    Bouquet(const Bouquet& other) : name(other.name), flowers(other.flowers) {
-        cout << "Bouquet copy constructor called for " << name << endl;
-    }
+    void addFlower(const Flower& flower);
+    void displayBouquet() const;
+    std::string getName() const;
+    double getTotalPrice() const;
 
-    // Оператор присвоєння копіюванням
-    Bouquet& operator=(const Bouquet& other) {
-        cout << "Bouquet assignment operator called for " << name << endl;
-        if (this != &other) {
-            name = other.name;
-            flowers = other.flowers; // Deep copy
-        }
-        return *this;
-    }
-
-    // Деструктор
-    virtual ~Bouquet() {
-        cout << "Bouquet destructor called for " << name << endl;
-    }
-
-    // Метод для додавання квітки до букета
-    void addFlower(const Flower& flower) {
-        flowers.push_back(flower);
-        cout << "Flower added to bouquet: " << name << endl;
-    }
-
-    // Метод для виведення інформації про букет
-    void displayBouquet() const {
-        cout << "Bouquet: " << name << endl;
-        if (flowers.empty()) {
-            cout << "Bouquet is empty." << endl;
-        } else {
-            cout << "Bouquet composition:" << endl;
-            for (const auto& flower : flowers) {
-                flower.displayInfo();
-            }
-        }
-    }
-
-    string getName() const { return name; } // Гетер для назви букета
-    double getTotalPrice() const { // Метод для розрахунку загальної вартості букета
-        double totalPrice = 0.0;
-        for(const auto& flower : flowers) {
-            totalPrice += flower.getPrice();
-        }
-        return totalPrice;
-    }
 };
 
 #endif // BOUQUET_H
