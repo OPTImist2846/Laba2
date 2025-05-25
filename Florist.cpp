5ea4dba50e3e74ebb4ebf5a2d24d2446734be201
OPTImist2846 <olessolja@gmail.com>
2025-05-26 00:07:21 +0300

    add Florist.cpp

--- Florist.cpp
+++ Florist.cpp
@@ -1,5 +1,43 @@
-//
-// Created by oless on 4/27/2025.
-//
+#include "Florist.h" // Включаємо власний заголовок
 
-#include "Florist.h"
+Florist::Florist() : name("Unnamed florist") {
+    // std::cout << "Florist default constructor" << std::endl;
+}
+
+Florist::Florist(const std::string& name) : name(name) {
+    // std::cout << "Florist constructor with name: " << this->name << std::endl;
+}
+
+Florist::Florist(const Florist& other) : name(other.name) {
+    // std::cout << "Florist copy constructor" << std::endl;
+}
+
+Florist& Florist::operator=(const Florist& other) {
+    // std::cout << "Florist assignment operator" << std::endl;
+    if (this != &other) {
+        name = other.name;
+    }
+    return *this;
+}
+
+Florist::~Florist() {
+    // std::cout << "Florist destructor: " << name << std::endl;
+}
+
+Bouquet Florist::createBouquet(const std::string& bouquetName, const std::vector<Flower>& flowerList) const {
+    std::cout << "Florist " << name << " is creating bouquet: " << bouquetName << std::endl;
+    Bouquet bouquet(bouquetName);
+    for (const auto& flower : flowerList) {
+        bouquet.addFlower(flower);
+    }
+    return bouquet;
+}
+
+void Florist::displayName() const {
+    std::cout << "Florist: " << name << std::endl;
+}
+
+std::string Florist::getName() const {
+    return name;
+}
+
--- Florist.h
+++ Florist.h
@@ -13,54 +13,16 @@ private:
    string name;
 
 public:
-   // Конструктор за замовчуванням
-   Florist() : name("Unnamed florist") {
-      cout << "Florist default constructor" << endl;
-   }
 
-   // Конструктор з ім'ям флориста
-   Florist(const string& name) : name(name) {
-      cout << "Florist constructor with name: " << this->name << endl;
-   }
+   Florist();
+   Florist(const string& name);
+   Florist(const Florist& other);
+   Florist& operator=(const Florist& other);
+   ~Florist();
 
-   // Конструктор копiювання
-   Florist(const Florist& other) : name(other.name)
-   {
-      cout << "Florist copy constructor" << endl;
-   }
-
-   // Оператор присвоєння копіюванням
-   Florist& operator=(const Florist& other) {
-      cout << "Florist assignment operator" << endl;
-      if (this != &other) {
-         name = other.name;
-      }
-      return *this;
-   }
-   // Деструктор
-   ~Florist() {
-      cout << "Florist destructor: " << name << endl;
-   }
-
-   // Метод для створення букета
-   Bouquet createBouquet(const string& bouquetName, const vector<Flower>& flowerList) const {
-      cout << "Florist " << name << " is creating bouquet: " << bouquetName << endl;
-      Bouquet bouquet(bouquetName); // Create the bouquet with the given name
-      for (const auto& flower : flowerList) {
-         bouquet.addFlower(flower); // Add flowers to the bouquet
-      }
-      return bouquet;
-   }
-
-   // Метод для виведення імені флориста
-   void displayName() const {
-      cout << "Florist: " << name << endl;
-   }
-
-   // Гетер для імені флориста
-   string getName() const {
-      return name;
-   }
+   Bouquet createBouquet(const string& bouquetName, const vector<Flower*>& flowerPointers) const; // Змінено на вектор покажчиків
+   void displayName() const;
+   string getName() const;
 };
 
 #endif // FLORIST_H
